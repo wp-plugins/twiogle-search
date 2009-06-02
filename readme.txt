57a262ccb51f5ca2a143f697a6227b20612bbcde
benjiballin <benjiballin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-02 22:09:27 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/twiogle-search/trunk@122443 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Plugin Name ===
+Contributors: benjiballin, armastevs
+Donate link: http://twiogle.com/
+Tags: google, twitter, twitter search, google search
+Requires at least: 2.0.2
+Tested up to: 2.1
+Stable tag: 4.3
+
+== Description ==
+
+This adds a widget that lets you search from Twitter and Google at the same time.  Displaying Twitter Search results on the right
+and Google Search Results on the left.
+
+== Installation ==
+
+1.  Simply Copy the plugin to your plugin folder and active it.  
+2.  Then Add the widget in the Appearance section.
---
+++ wp_twiogle.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Twiogle Search Widgit
+Plugin URI: http://twiogle.com
+Description: This adds a widgit so you can search Google and Twitter at the same time.
+Version: 1.0
+Author: Benjiballin
+Author URI: http://twiogle.com
+*/
+
+function wp_twiogle_widget() {
+?>
+<h2 class="widgettitle">Twiogle Search</h2>
+<form onSubmit="openNew();" id="cse-search-box">
+
+<input type="text" id="q" size="15"/>
+<button onclick="javascript: openNew();">Twiogle it!</button>
+</form>
+
+<script type="text/javascript">
+function openNew()
+{
+window.open('http://twiogle.com?q=' + document.getElementById('q').value, '_blank');
+}
+</script>
+<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=cse-search-box&amp;lang=en"></script>
+
+<?php
+}
+function wp_twiogle_init()
+{
+register_sidebar_widget(__('Twiogle Search'), 'wp_twiogle_widget');
+}
+add_action("plugins_loaded", "wp_twiogle_init");
+?>
